0c74bd86551ca8acfd29bed0543a99561a27d5ed
Lorenzo Costa <costa86@zoho.com>
2022-08-19 15:11:46 +0100

    yes

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use copypasta::{ClipboardContext, ClipboardProvider};
+use std::io;
+
+fn main() {
+    let mut result = String::new();
+
+    loop {
+        let mut input = String::new();
+        let stdin = io::stdin().read_line(&mut input);
+
+        match stdin {
+            Ok(x) => {
+                input = input.trim().to_string();
+                if x == 0 {
+                    break;
+                }
+                result.push_str("\n");
+                result.push_str(&input);
+            }
+            Err(_) => {
+                panic!("Could not read line")
+            }
+        }
+    }
+    set_clipboard(&result);
+    println!("Clipboard updated");
+}
+
+fn set_clipboard(content: &str) {
+    let mut ctx = ClipboardContext::new().unwrap();
+    ctx.set_contents(content.to_string().to_owned()).unwrap();
+    ctx.get_contents().unwrap();
+}
